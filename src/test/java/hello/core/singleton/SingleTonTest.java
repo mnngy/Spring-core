31995b6f5986a04ee4bfe9696b5bb6fef40ef68c
mnngy <59257197+mnngy@users.noreply.github.com>
2021-04-03 15:25:44 +0900

    싱글톤 패턴
    1. 싱글톤 패턴을 구현하는 간단한 방법
    
    2. 싱글톤 패턴의 문제점

--- src/test/java/hello/core/singleton/SingleTonTest.java
+++ src/test/java/hello/core/singleton/SingleTonTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SingleTonTest {
 
     @Test
@@ -23,6 +25,20 @@ public class SingleTonTest {
         System.out.println("memberService2 = " + memberService2);
 
         // memberService != memberService2
-        Assertions.assertThat(memberService).isNotSameAs(memberService2);
+        assertThat(memberService).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        // same ==
+        // equal
     }
 }
